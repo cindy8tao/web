e08ee11a6d469508db6c67257439f2621a96a9e9
cindy8tao <cindy8tao@gmail.com>
2023-05-20 16:49:53 -0400

    Revert style back

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,19 +4,28 @@ const About = () => {
   return (
     <div name='about' className='w-full h-screen bg-[#e0eff4] text-gray-600'>
       <div className='flex flex-col justify-center items-center w-full h-full'>
-        <div className='max-w-[1000px] w-full px-4'>
-          <div className='text-4xl font-bold text-gray-600 text-center mb-8'>
-            <span className='border-b-4 border-[#E27D60]'>About</span>
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+          <div className='sm:text-right pb-8 pl-4'>
+            <p className='text-4xl font-bold inline border-b-4 border-[#E27D60]'>
+              About
+            </p>
           </div>
-          <div className='grid grid-cols-1 sm:grid-cols-2 gap-8'>
-            <div className='sm:text-right'>
-              <p className='text-2xl font-bold'>Welcome to my personal website! I'm Cindy Tao, and I'm excited to share my journey as a software engineer with you.</p>
-            </div>
-            <div>
-              <p>
-                I'm a skilled software engineer with a versatile background in developing scalable applications. Proficient in Java, Python, and C++, I excel in collaborating with cross-functional teams to deliver innovative solutions. With expertise in AWS, Azure, and database ecosystems, I constantly strive to stay at the forefront of technology. Committed to problem-solving and meticulous in my approach, I bring a diverse perspective and a strong attention to detail to every project. My goal is to leverage technology to make a positive impact and drive innovation.
-              </p>
-            </div>
+          <div></div>
+        </div>
+        <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+          <div className='sm:text-right text-4xl font-bold'>
+            <p>
+              Welcome to my personal website! I'm Cindy Tao, and
+              I'm excited to share my journey as a software engineer with you.</p>
+          </div>
+          <div>
+            <p>
+              I'm a skilled software engineer with a versatile background in developing scalable applications.
+              Proficient in Java, Python, and C++, I excel in collaborating with cross-functional teams to
+              deliver innovative solutions. With expertise in AWS, Azure, and database ecosystems, I constantly
+              strive to stay at the forefront of technology. Committed to problem-solving and meticulous in my
+              approach, I bring a diverse perspective and a strong attention to detail to every project. My goal
+              is to leverage technology to make a positive impact and drive innovation.</p>
           </div>
         </div>
       </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,41 +10,37 @@ const HomePage = () => {
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full relative'>
         <div className='z-10'>
-          <h1 className='text-2xl text-gray-600 mt-10 sm:text-4xl'>
-            Hi there, my name is
-          </h1>
+          <h1 className='text-2xl text-gray-600 mt-10'>Hi there, my name is</h1>
           <h1 className='text-4xl sm:text-6xl font-bold text-[#5085A5]'>
             CINDY TAO
           </h1>
-          <h2 className='text-3xl sm:text-4xl font-bold text-[#8FC1E3]'>
+          <h2 className='text-4xl sm:text-4xl font-bold text-[#8FC1E3]'>
             I'm a
           </h2>
-          <div className='flex flex-col sm:flex-row'>
-            <div className='flex-1'>
-              <h2 className='text-2xl sm:text-3xl font-bold text-[#E27D60]'>
-                <Typewriter
-                  options={{
-                    strings: ['Software Engineer', 'Problem Solver', 'Analytical Thinker', 'Team Player'],
-                    autoStart: true,
-                    loop: true,
-                  }}
-                />
-              </h2>
+          <h2 className='text-2xl sm:text-2xl font-bold text-[#E27D60]'>
+            <Typewriter
+              options={{
+                strings: ['Software Engineer', 'Problem Solver', 'Analytical Thinker', 'Team Player'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
+          <div className='flex'>
+            <div>
               <p className='text-gray-600 py-4 max-w-[700px]'>
                 As a software engineer, I am passionate about creating innovative solutions through coding and problem-solving. I enjoy collaborating with teams to develop robust and scalable software applications, using my technical expertise and attention to detail to deliver high-quality products that meet user needs and drive positive impact.
               </p>
-              <div className="flex flex-col sm:flex-row items-start sm:items-center">
-                <button className="text-black group border-2 px-6 py-3 my-2 sm:mr-3 flex items-center hover:bg-[#E27D60] hover:border-[#E27D60]">
-                  <a href={CindyResume2023} target="_blank" rel="noopener noreferrer">
-                    View Resume (PDF)
-                  </a>
-                  <span className="group-hover:rotate-90 duration-300">
-                    <HiArrowNarrowRight className="ml-3" />
-                  </span>
-                </button>
-              </div>
+              <button className="text-black group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#E27D60] hover:border-[#E27D60]">
+                <a href={CindyResume2023} target="_blank" rel="noopener noreferrer">
+                  View Resume (PDF)
+                </a>
+                <span className="group-hover:rotate-90 duration-300">
+                  <HiArrowNarrowRight className="ml-3" />
+                </span>
+              </button>
             </div>
-            <img src={self1} alt='Self 1' className='ml-4 w-[250px] h-[500px]' />
+            <img src={self1} alt='Self 1' className='ml-4 w-[250px] h-[500px] mt-[-20%]' />
           </div>
         </div>
       </div>
